cb34586902a19b5f7aff2506889b00a349c5c6d5
tayfunetta <tayfunemrekahraman@gmail.com>
2023-11-06 23:58:38 +0300

    completed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { PiInfinityFill, PiLinkBold, PiUserCircleBold } from "react-icons/pi";
 
-export function Header() {
-  return <div className="col-span-5 h-16 bg-white rounded-2xl">Header</div>;
+interface HeaderProps {
+  handleDashboard: (val: boolean) => void;
+  isDashboard: boolean;
+}
+
+export function Header({ isDashboard, handleDashboard }: HeaderProps) {
+  console.log(isDashboard)
+  return (
+    <header className="relative col-span-5 flex justify-center items-center gap-x-8 h-16 bg-white rounded-2xl">
+      <div className="absolute flex items-center left-5">
+        <PiInfinityFill className="w-10 h-10 text-green-600" />{" "}
+        <span className="text-3xl font-black tracking-tighter">devlinks</span>
+      </div>
+      <button
+        className={`flex items-center gap-x-2 text-green-900 ${isDashboard ? "bg-green-300": "bg-green-50"} py-2 px-3 rounded-lg`}
+        onClick={() => handleDashboard(true)}
+      >
+        <PiLinkBold /> Links
+      </button>
+      <button
+        className={`flex items-center gap-x-2 text-green-900 ${isDashboard ? "bg-green-50" : "bg-green-300"}  py-2 px-3 rounded-lg`}
+        onClick={() => handleDashboard(false)}
+      >
+        <PiUserCircleBold /> Profile Details
+      </button>
+    </header>
+  );
 }
