5c36e726c154a4405dc65c90e5d828d3adc03771
tayfunetta <tayfunemrekahraman@gmail.com>
2023-11-09 18:47:22 +0300

    Refactor Alert component to use conditional rendering

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -8,25 +8,27 @@ interface AlertProps {
 
 export const Alert = ({ y, alertType: { status } }: AlertProps) => {
   return (
-    typeof status === "string" &&
-    (status === "warning" ? (
-      <motion.div
-        className="fixed right-5 flex items-center gap-x-2 p-4 bg-red-600 text-gray-100 text-sm rounded-lg shadow-lg shadow-gray-600"
-        initial={{ y: "100vh" }}
-        animate={{ y }}
-      >
-        <FaExclamation className="w-4 h-4" /> Please make sure you have filled
-        out all fields correctly.
-      </motion.div>
-    ) : (
-      <motion.div
-        className="fixed right-5 flex items-center gap-x-2 p-4 bg-green-600 text-gray-100 text-sm rounded-lg shadow-lg shadow-gray-600"
-        initial={{ y: "100vh" }}
-        animate={{ y }}
-      >
-        <FaCheck className="w-4 h-4" /> Your changes have been successfully
-        saved.
-      </motion.div>
-    ))
+    <>
+      {status === "warning" && (
+        <motion.div
+          className="fixed right-5 flex items-center gap-x-2 p-4 bg-red-600 text-gray-100 text-sm rounded-lg shadow-lg shadow-gray-600"
+          initial={{ y: "100vh" }}
+          animate={{ y }}
+        >
+          <FaExclamation className="w-4 h-4" /> Please make sure you have filled
+          out all fields correctly.
+        </motion.div>
+      )}
+      {status === "success" && (
+        <motion.div
+          className="fixed right-5 flex items-center gap-x-2 p-4 bg-green-600 text-gray-100 text-sm rounded-lg shadow-lg shadow-gray-600"
+          initial={{ y: "100vh" }}
+          animate={{ y }}
+        >
+          <FaCheck className="w-4 h-4" /> Your changes have been successfully
+          saved.
+        </motion.div>
+      )}
+    </>
   );
 };
